b25354b7ee9130d06a5939d3765b67614d1aa7bb
David Li <shuai_li@neimanmarcus.com>
2019-05-03 00:07:27 -0500

    1. Add Redux 1.1 Add two stores for server and client 1.2 Add user action and reducer 1.3 Add userlist component 1.4 Add polyfiller

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import Home from './components/Home';
+import UsersList from './components/UsersList';
 
 export default() => {
     return (
@@ -8,6 +9,7 @@ export default() => {
             <Route exact path="/" component={Home} />
             {/* <Route exact path="/" component={() => 'hi' } /> */}
             <Route path="/hi" component={() => 'hi'} />
+            <Route path="/users" component={UsersList} />
         </div>
     )
 }
